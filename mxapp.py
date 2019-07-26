5f85d8c83a7105f142fdba4e9fd37d3bc46f2e15
yadlra <yadi.sb1@gmail.com>
2019-07-26 22:05:08 +0100

    Add about route and edit home route

--- mxapp.py
+++ mxapp.py
@@ -2,5 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hola!</h1>'
+@app.route('/home')
+def home():
+    return '<h1>Hola, este es el comienzo!</h1>'
+
+@app.route('/about')
+def about():
+    return '<h1>About page!</h1>'
